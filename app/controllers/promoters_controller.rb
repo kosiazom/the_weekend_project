772072520a35a8d74372b5ed8b5e40b220aee336
Kosi Akporji <kosiazom@gmail.com>
2020-07-07 23:03:03 -0400

    created promoter index and show page

--- app/controllers/promoters_controller.rb
+++ app/controllers/promoters_controller.rb
@@ -1,2 +1,11 @@
 class PromotersController < ApplicationController
+
+    def index
+        @promoters = Promoter.all
+    end
+    
+    def show
+        @promoter = Promoter.find(params[:id])
+    end
+    
 end
--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -1,4 +1,10 @@
 class Promoter < ApplicationRecord
     has_many :parties
+
+    def promoter_party
+        self.parties.map do|party|
+            party.name
+        end.join
+    end
     
 end

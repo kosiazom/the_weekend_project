097d54a8c2c1cd53d269e3219ca19187e107b2ea
Kosi Akporji <kosiazom@gmail.com>
2020-07-08 08:19:35 -0400

    updated promoter routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   resources :guest_lists
   resources :djs, only: [:index, :show]
   resources :party_goers
-  resources :promoters
+  resources :promoters, only: [:index, :show]
   
   post '/parties/:party_id/party_goers', to: 'party_goers#attend', as: 'attend_party'
   
   
   resources :parties, only: [:index, :show] do
-  resources :reviews, except: [:index, :show]
-  end
+    resources :reviews, except: [:index, :show]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
